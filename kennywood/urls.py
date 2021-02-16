a2f3f091d57555bb9a25c92319c44bd364314c2b
Nick Walters <nswalters.s@gmail.com>
2021-02-16 17:03:01 -0600

    Adds '/login' endpoint to allow a user to login

--- kennywood/urls.py
+++ kennywood/urls.py
@@ -2,10 +2,11 @@ from django.conf.urls import include
 from django.urls import path
 #from rest_framework import routers
 
-from kennywoodapi.views import register_user
+from kennywoodapi.views import register_user, login_user
 
 #router = routers.DefaultRouter(trailing_slash=False)
 
 urlpatterns = [
     path('register', register_user),
+    path('login', login_user),
 ]
