1081f50c09236bd97db90a0a028eedafa771c1e0
Nick Walters <nswalters.s@gmail.com>
2021-02-16 13:27:38 -0600

    Completed fixtures and seed data script

--- kennywoodapi/models/__init__.py
+++ kennywoodapi/models/__init__.py
@@ -4,3 +4,4 @@ from .attraction_rating import AttractionRating
 from .attraction_category import AttractionCategory
 from .park_area import ParkArea
 from .target_population import TargetPopulation
+from .itinerary import Itinerary
